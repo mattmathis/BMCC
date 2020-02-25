d54ddf55b39b65381f5568f392c802d659f02852
mattmathis <mattmathis@mattmathis-glaptop.roam.corp.google.com>
2020-02-24 19:52:47 -0800

    Checkpoint after 2019

--- AuditRoster/getroster.sh
+++ AuditRoster/getroster.sh
@@ -1,4 +1,22 @@
 #! /bin/bash
 
-roster="https://docs.google.com/spreadsheets/d/1h_dcOkk0xgs3XT0YqBieEPjJ0wYqXBOHXU76NV-pghs"
-wget "$roster/export?format=csv" -O roster.csv
+# Exported googlegroup
+mailinglist=contraburners2019.csv
+listfile=$HOME/Downloads/$mailinglist
+
+# Roster Google docs in successive years
+roster17="https://docs.google.com/spreadsheets/d/1h_dcOkk0xgs3XT0YqBieEPjJ0wYqXBOHXU76NV-pghs"
+roster18="https://docs.google.com/spreadsheets/d/1QaukY9NbAVqL5CKkOrDjGVKcS6D56lDWO_LmOkuXh98"
+roster19="https://docs.google.com/spreadsheets/d/1tRnJuQ7-NdI_9wimOBFgRYFeL8akyfgf4YO4OnHHvBo"
+
+echo "Fetch Roster"
+wget "$roster19/export?format=csv" -O roster.csv
+
+if [ -f $listfile ]; then
+  echo "Updating mailing list"
+  mv $listfile .
+else
+  echo "Keeping existing previously fetched mailing list"
+fi
+
+./mergelists.py
--- AuditRoster/mergelists.py
+++ AuditRoster/mergelists.py
@@ -5,8 +5,9 @@
 """
 
 import csv
+import collections
 
-invited = "0123"
+invited = "01234"
 
 def ok(dgs):
   if not dgs:
@@ -23,31 +24,43 @@ def main():
   roster = csv.DictReader(f)
   rows=[]
   rosteraddrs = {}
+  addrcounts = collections.Counter()
   for r in roster:
     if not r['Name']:
       continue
     rows.append(r)
-    rosteraddrs[r['Preferred email address'].lower()] = r['Ticket status']
-    rosteraddrs[r['Google email address'].lower()] = r['Ticket status']
-
+    for col in {'Preferred email address', 'Google email address'}:
+      addr = r[col].lower()
+      if addr == '':
+        continue
+      rosteraddrs[addr] = r['Ticket status']
+      addrcounts[addr] += 1
 
   # read to googlegroup members
-  f = open("contraburners2017.csv", 'r')
+  f = open("contraburners2019.csv", 'r')
   f.readline() # discard the title
   email = csv.DictReader(f)
 
+  # Generate a gmail pattern match
+  pattern = ''
+  sep = '( '
+  for addr in rosteraddrs:
+    pattern += sep + addr
+    sep = ' OR '
+  pattern += ' )'
+  print 'Gmail match: '+pattern
 
   listaddrs = {}
-  print "Google group members registration statuses:"
+  print "Google group member registration statuses:"
   for l in email:
     a = l['Email address'].lower()
     listaddrs[a] = True
     if a not in rosteraddrs:
       print "Not registered:  %s <%s>,"%(l['Nickname'], a)
     elif not ok(rosteraddrs[a]):
-      print "Extra:           %s <%s>,"%(l['Nickname'], a)
+      print "Extra:           %s <%s> (code:%s)"%(l['Nickname'], a, rosteraddrs[a])
     elif verbose:
-      print "Present:         %s <%s>,"%(l['Nickname'], a)
+      print "Present (%d):         %s <%s>,"%(addrcounts[a], l['Nickname'], a)
 
   print "Missing from Google group"
   for r in rows:
@@ -58,7 +71,7 @@ def main():
     if (a1 not in listaddrs) and (a2 not in listaddrs):
       if a2:
         print "  %s <%s>,"%(r['Name'], a2)
-      if a1:
+      else:
         print "  %s <%s>,"%(r['Name'], a1)
 
 if __name__ == "__main__":
